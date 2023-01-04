e6c9f505c9e7591ddbf9889bd490bd5842af0882
europe <mavridou.evi@outlook.com>
2023-01-04 11:40:51 +0100

    ich habe text geädert plus noch code

--- src/main/kotlin/1_Wiederholung.kt
+++ src/main/kotlin/1_Wiederholung.kt
@@ -53,4 +53,20 @@ println("MY NAME IS $nAME  $fIRST_NAME $lAST_NAME UND MEINE TELEFONUMMER IST  $t
     println("wIR Sicken das essen um $tIME in Ihre straße")
 
    println("Ich bin in $placeOfBirth ich komme aus $lastVacation")
-}
+
+
+        val fruits: List<String> = listOf("Banane", "Apfel", "Birne")
+        val fruit: String = fruits[fruits.size-3]
+        println(fruit)
+
+
+        /* hier wir haben eine liste mit name fruits und auf die liste gehort 2 allgemein ..(0,1,2,)
+        wenn wir jetzt -3 auf size haben dann die bringt raus die banane
+         */
+        val programm : List<String> = listOf("Corel","photoshop","kotlin")
+        val programm1 : String = programm[programm.size -2]
+
+        println("das programm was auf die erste stelle komm ist das $programm1")
+//dann komm photoshop
+
+    }
